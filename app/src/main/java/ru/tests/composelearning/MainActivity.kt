e41825c170c76c6691576efb51b6b410ee4b55c3
toffeantyri <50056081+toffeantyri@users.noreply.github.com>
2022-10-25 16:40:20 +0500

    simply list items

--- app/src/main/java/ru/tests/composelearning/MainActivity.kt
+++ app/src/main/java/ru/tests/composelearning/MainActivity.kt
@@ -3,50 +3,57 @@ package ru.tests.composelearning
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            twoColumnPreview()
+            Column {
+                listItem("Tony Shampan", "Programmer")
+                listItem("Tony Shampan2", "Programmer2")
+                listItem("Tony Shampan3", "Programmer3")
+                listItem("Tony Shampan4", "Programmer4")
+                listItem("Tony Shampan5", "Programmer5")
+                listItem("Tony Shampan6", "Programmer6")
+            }
         }
     }
 }
 
-
-@Preview
 @Composable
-fun twoColumnPreview() {
-    Row(
-        modifier = Modifier.background(Color.Gray).fillMaxWidth().fillMaxHeight(.5f),
-        horizontalArrangement = Arrangement.SpaceAround,
-        verticalAlignment = Alignment.CenterVertically
+private fun listItem(name: String, prof: String) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth().padding(horizontal = 10.dp, vertical = 7.dp),
+        elevation = 5.dp, shape = RoundedCornerShape(10.dp)
     ) {
-        Column(
-            modifier = Modifier.background(Color.Green).fillMaxHeight().fillMaxWidth(.5f),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.SpaceEvenly
-        ) {
-            Text("Hello")
-            Text("Toffy")
-            Text("Test")
-        }
-        Column(
-            modifier = Modifier.background(Color.Red).fillMaxHeight().fillMaxWidth(1f),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.SpaceEvenly
-        ) {
-            Text("Hello")
-            Text("Toffy")
-            Text("Test")
+        Box() {
+            Row(modifier = Modifier.padding(5.dp), verticalAlignment = Alignment.CenterVertically) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_launcher_background),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(64.dp)
+                        .clip(CircleShape)
+                )
+                Column(modifier = Modifier.padding(start = 10.dp)) {
+                    Text(text = name)
+                    Text(text = prof)
+                }
+            }
+
         }
     }
 }
