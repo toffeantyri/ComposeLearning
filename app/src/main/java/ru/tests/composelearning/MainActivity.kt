99e78310b53b3f3417078228eba399ea813ba246
toffeantyri <50056081+toffeantyri@users.noreply.github.com>
2022-10-28 12:39:31 +0500

    LazyColumn (as Recycler) but without save state if item recycled

--- app/src/main/java/ru/tests/composelearning/MainActivity.kt
+++ app/src/main/java/ru/tests/composelearning/MainActivity.kt
@@ -6,10 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -26,24 +25,12 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column(
-                modifier = Modifier.verticalScroll(rememberScrollState())
+            LazyColumn(
             ) {
-                listItem("Tony Shampan", "Programmer")
-                listItem("Tony Shampan", "Programmer")
-                listItem("Tony Shampan", "Programmer")
-                listItem("Tony Shampan", "Programmer")
-                listItem("Tony Shampan", "Programmer")
-                listItem("Tony Shampan", "Programmer")
-                listItem("Tony Shampan", "Programmer")
-                listItem("Tony Shampan", "Programmer")
-                listItem("Tony Shampan", "Programmer")
-                listItem("Tony Shampan", "Programmer")
-                listItem("Tony Shampan", "Programmer")
-                listItem("Tony Shampan", "Programmer")
-                listItem("Tony Shampan", "Programmer")
-                listItem("Tony Shampan", "Programmer")
-                listItem("Tony Shampan", "Programmer")
+                items(count = 100) {
+                    listItem(name = "tony $it", prof = "developer")
+                }
+
             }
         }
     }
